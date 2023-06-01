17bff7cf5626c9e309ddea11a527c8d46e86da00
Iarlen <iarlensr@hotmail.com>
2023-06-01 17:09:11 -0300

    feat: displaying pokemon pokemon data dynamically.

--- app/details.tsx
+++ app/details.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text, View, TouchableOpacity, Image } from 'react-native'
 import { useLocalSearchParams, useRouter } from 'expo-router'
 import { AntDesign } from '@expo/vector-icons'
 
-import Pokemon from '../src/assets/32.png'
+import { useGetPokemon } from '../src/hooks/useGetPokemon'
 
 const Details = () => {
   const { id } = useLocalSearchParams()
   const router = useRouter()
 
+  const { getAPokemon, pokemon } = useGetPokemon()
+
+  useEffect(() => {
+    getAPokemon(String(id))
+  }, [id, getAPokemon])
+
   const randleBackToHome = () => {
     router.push('/')
   }
+
   return (
     <View className="mt-5">
       <View className="w-full">
@@ -23,10 +30,18 @@ const Details = () => {
         </TouchableOpacity>
       </View>
       <View className="mt-4 w-full items-center justify-center">
-        <Image source={Pokemon} alt="pokemon" className="h-[350] w-[300]" />
+        <Image
+          source={{
+            uri: `${pokemon && pokemon.sprites.other.home.front_default}`,
+          }}
+          alt="pokemon"
+          className="h-[300] w-[300]"
+        />
       </View>
       <View className="w-full items-center justify-center">
-        <Text className="my-3 text-4xl text-white">{id}</Text>
+        <Text className="my-3 text-4xl text-white">
+          {pokemon && pokemon.species.name}
+        </Text>
         <View className="flex-row items-center justify-between gap-10">
           <Text className="text-xl text-white">Ataque: 40</Text>
           <Text className="text-xl text-white">Defesa: 40</Text>
@@ -35,9 +50,15 @@ const Details = () => {
       <View className="mt-4 w-full">
         <Text className="text-xl text-white">Habilidades:</Text>
         <View className="mt-1">
-          <Text className="text-base text-white">- poison-point</Text>
-          <Text className="text-base text-white">- rivalry</Text>
-          <Text className="text-base text-white">- hustle</Text>
+          {pokemon &&
+            pokemon.abilities.map((ability) => (
+              <Text
+                className="text-base capitalize text-white"
+                key={ability.ability.name}
+              >
+                - {ability.ability.name}
+              </Text>
+            ))}
         </View>
       </View>
     </View>
