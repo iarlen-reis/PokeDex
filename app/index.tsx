fb628d9dc6e1e1224b5e09dc3f210dd289bb2d4d
Iarlen <iarlensr@hotmail.com>
2023-06-01 05:02:59 -0300

    styles: finalizing the styling of the Index page.

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { FlatList } from 'react-native'
+import Card from '../src/components/Card'
+import { useGetPokemons } from '../src/hooks/useGetPokemon'
+
+const Index = () => {
+  const { pokemons } = useGetPokemons()
+
+  return (
+    <FlatList
+      data={pokemons}
+      renderItem={({ item }) => (
+        <Card id={item.id.toString()} name={item.name} />
+      )}
+      keyExtractor={(item) => item.id.toString()}
+      showsVerticalScrollIndicator={false}
+      className="mt-5 w-full rounded-xl"
+    />
+  )
+}
+
+export default Index
