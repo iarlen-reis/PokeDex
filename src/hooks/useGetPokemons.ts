924d25e5808eaf822b02f63833869a711e12cd95
Iarlen <iarlensr@hotmail.com>
2023-06-02 14:26:32 -0300

    feat: creating loadMorePokemon functionality to display more pokemons when reaching the end of the scroll.

--- src/hooks/useGetPokemons.ts
+++ src/hooks/useGetPokemons.ts
@@ -8,10 +8,14 @@ interface IPokemon {
 
 interface IUseGetPokemons {
   pokemons: IPokemon[]
+  loadMorePokemon: () => Promise<void>
+  loading: boolean
 }
 
 export const useGetPokemons = (): IUseGetPokemons => {
   const [pokemons, setPokemons] = useState<IPokemon[]>([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const getPokemons = async () => {
@@ -27,11 +31,33 @@ export const useGetPokemons = (): IUseGetPokemons => {
         setPokemons(data)
       } catch (error) {
         console.log(error)
+      } finally {
+        setLoading(false)
       }
     }
 
     getPokemons()
-  }, []) // Executa apenas na montagem do componente
+  }, [])
 
-  return { pokemons }
+  const loadMorePokemon = async () => {
+    try {
+      const response = await api.get(
+        `/pokemon?limit=20&offset=${currentPage * 20}`,
+      )
+      const data = response.data.results as IPokemon[]
+
+      data.forEach((item, index) => {
+        item.id = index + currentPage * 20 + 1
+      })
+
+      setPokemons((prevList) => [...prevList, ...data])
+      setCurrentPage((prevPage) => prevPage + 1)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { pokemons, loadMorePokemon, loading }
 }
