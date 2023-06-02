063cdef651118eac3d92a28afff9beda02a7bd20
Iarlen <iarlensr@hotmail.com>
2023-06-01 22:53:29 -0300

    refactor: changing the link to go to the individual pokemon page by a button on the Card component.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { View, Text, Image } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
-import { Link } from 'expo-router'
+import { useRouter } from 'expo-router'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 interface IPokemon {
   id: string
@@ -10,30 +11,32 @@ interface IPokemon {
 }
 
 const Card = (props: IPokemon) => {
+  const router = useRouter()
+
+  const randleToPokemon = () => {
+    router.push({ pathname: 'details', params: { id: props.id } })
+  }
   return (
     <View className="mb-5 h-60 w-full max-w-sm flex-row rounded-xl bg-slate-500 p-2">
-      <View className="w-[60%] items-center justify-center">
+      <View className="w-[50%] items-center justify-center">
         <Image
           source={{
             uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${props.id}.png`,
           }}
           alt={props.name}
-          className="h-full w-full"
+          className="h-[80%] w-[100%]"
         />
       </View>
-      <View className="w-[40%] items-center justify-between">
-        <Text className="text-2xl text-white">{props.name}</Text>
+      <View className="w-[50%] items-center justify-between">
+        <Text className="text-2xl capitalize text-white">{props.name}</Text>
 
-        <View className="mb-5 h-20 w-20 items-center justify-center rounded bg-purple-400">
-          <Link
-            href={{
-              pathname: 'details',
-              params: { id: props.id },
-            }}
-          >
-            <Feather name="search" color="#FFF" size={30} />
-          </Link>
-        </View>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          className="mb-5 h-20 w-20 items-center justify-center rounded bg-purple-400"
+          onPress={randleToPokemon}
+        >
+          <Feather name="search" color="#FFF" size={30} />
+        </TouchableOpacity>
       </View>
     </View>
   )
