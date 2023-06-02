3ee88b3010a05e028b3d0fc3ab51164f900371f2
Iarlen <iarlensr@hotmail.com>
2023-06-02 14:27:16 -0300

    feat: adding loading status when loading the page and requesting new pokemons.

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,33 @@
-import { FlatList } from 'react-native'
+import { ActivityIndicator, FlatList, View } from 'react-native'
 import Card from '../src/components/Card'
 import { useGetPokemons } from '../src/hooks/useGetPokemons'
 
 const Index = () => {
-  const { pokemons } = useGetPokemons()
+  const { pokemons, loadMorePokemon, loading } = useGetPokemons()
+
+  const handleLoadMore = () => {
+    loadMorePokemon()
+  }
 
   return (
-    <FlatList
-      data={pokemons}
-      renderItem={({ item }) => (
-        <Card id={item.id.toString()} name={item.name} />
+    <>
+      <FlatList
+        data={pokemons}
+        renderItem={({ item }) => (
+          <Card id={item.id.toString()} name={item.name} />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        showsVerticalScrollIndicator={false}
+        className="mt-5 w-full rounded-xl"
+        onEndReached={handleLoadMore}
+        onEndReachedThreshold={0.5}
+      />
+      {loading && (
+        <View className="items-center justify-center bg-transparent">
+          <ActivityIndicator size="large" />
+        </View>
       )}
-      keyExtractor={(item) => item.id.toString()}
-      showsVerticalScrollIndicator={false}
-      className="mt-5 w-full rounded-xl"
-    />
+    </>
   )
 }
 
